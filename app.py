405fb62d97053447300d2873455816dcac97eb6b
fedyaxudayarov-create <fedyaxudayarov@gmail.com>
2026-02-13 18:59:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from flask import Flask, render_template, request, jsonify
 # app.py ичидаги мана шу қаторни топинг ва алмаштиринг:
 API_KEY = "AIzaSyADOBxK551UG6yAyl_u3o_VFm0bSRKv6YY"
 genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
-
+# 9-қаторни шундай ёзинг:
+model = genai.GenerativeModel('models/gemini-1.5-flash-latest')
 BASE_DIR = Path(__file__).resolve().parent
 DB_PATH = BASE_DIR / "data" / "lex_index.db"
 
@@ -76,6 +76,7 @@ def api_search():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
 
 
